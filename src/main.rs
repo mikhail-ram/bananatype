6c5f80f3d2cbb9bc31d634a5779ac56c8f883e5c
Mikhail Ram <mikhailethanram@gmail.com>
2021-11-01 19:50:56 +0530

    Using termion as backend for user interface

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,102 @@
-use std::fs::File;
-use std::io::{ BufReader, BufRead, self };
-use std::{ string::ToString, time::{ Duration, Instant } };
-use crossterm::{ execute, terminal, cursor, event::{ read, Event, KeyCode }, style::{ self, Colorize } };
-use rand::{ distributions::Uniform, Rng };
+use rand::{distributions::Uniform, Rng};
+use std::fs;
+use std::io::{self, stdin, stdout, BufRead, Write};
+use termion::color;
+use termion::cursor::DetectCursorPos;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
-fn main() {
-    let file = File::open("src/words.txt").unwrap();
-    let file: Vec<String> = BufReader::new(file).lines().filter_map(io::Result::ok).collect();
-    let mut stdout = io::stdout();
-    let mut current_word = String::new();
-    let mut word_index = 0;
-    let mut correct_to_incorrect = vec![0; file.len()];
-
-    execute!(stdout, terminal::Clear(terminal::ClearType::All), cursor::MoveTo(0, 0)).unwrap();
-
-    for (i, word) in file.iter().enumerate() {
-        if correct_to_incorrect[i] == 1 {
-            execute!(stdout, style::PrintStyledContent(format!("{} ", word).green())).unwrap();
-        }
-        else if correct_to_incorrect[i] == -1 {
-            execute!(stdout, style::PrintStyledContent(format!("{} ", word).red())).unwrap();
-        }
-        else {
-            execute!(stdout, style::Print(format!("{} ", word))).unwrap();
+struct Text {
+    text: String,
+    current_word: usize,
+}
+impl Text {
+    fn new(filepath: &str) -> Text {
+        let text = Text::generate_text(filepath);
+        Text {
+            text,
+            current_word: 0,
         }
     }
-    println!();
-    println!();
+    fn generate_text(filepath: &str) -> String {
+        let file = fs::File::open(filepath).unwrap();
+        let file: Vec<String> = io::BufReader::new(file)
+            .lines()
+            .filter_map(io::Result::ok)
+            .collect();
+        let range = Uniform::from(0..file.len());
+        let rand_nums: Vec<usize> = rand::thread_rng().sample_iter(range).take(100).collect();
+        let file: String = rand_nums
+            .into_iter()
+            .map(|n| file[n].clone())
+            .collect::<Vec<String>>()
+            .join(" ");
+        file
+    }
+}
 
-
+fn main() {
+    let file = Text::new("src/words.txt");
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+    write!(
+        stdout,
+        "{}{}{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        file.text,
+        termion::cursor::Goto(1, 1),
+    )
+    .unwrap();
+    stdout.flush().unwrap();
+    let mut keys = stdin.keys();
+    let mut position: usize = 0;
     loop {
-        // `read()` blocks until an `Event` is available
-        if let Event::Key(c) = read().unwrap() {
-            match c.code {
-                KeyCode::Char(d) => {
-                    if d != ' ' {
-                        current_word.push(d);
-                        if file[word_index].starts_with(current_word.as_str()) {
-                            execute!(stdout, style::Print(d)).unwrap();
-                        }
-                        else {
-                            execute!(stdout, style::PrintStyledContent(d.to_owned().red())).unwrap();
-                        }
+        let c = keys.next().unwrap().unwrap();
+        match c {
+            Key::Char(a) => {
+                if a == ' ' {
+                    if a == file.text.chars().nth(position).unwrap() {
+                        write!(
+                            stdout,
+                            "{}{}",
+                            color::Fg(color::Green),
+                            file.text.chars().nth(position).unwrap()
+                        );
+                    } else {
+                        write!(
+                            stdout,
+                            "{}{}",
+                            color::Fg(color::Red),
+                            file.text.chars().nth(position).unwrap()
+                        );
                     }
-                    else {
-                        if current_word == file[word_index] {
-                            correct_to_incorrect[word_index] = 1;
-                        }
-                        else {
-                            correct_to_incorrect[word_index] = -1;
-                        }
-                        word_index += 1;
-                        current_word.clear();
-                        execute!(stdout, terminal::Clear(terminal::ClearType::All), cursor::MoveTo(0, 0)).unwrap();
-                        for (i, word) in file.iter().enumerate() {
-                            if correct_to_incorrect[i] == 1 {
-                                execute!(stdout, style::PrintStyledContent(format!("{} ", word).green())).unwrap();
-                            }
-                            else if correct_to_incorrect[i] == -1 {
-                                execute!(stdout, style::PrintStyledContent(format!("{} ", word).red())).unwrap();
-                            }
-                            else {
-                                execute!(stdout, style::Print(format!("{} ", word))).unwrap();
-                            }
-                        }
-                        println!();
-                        println!();
+                } else {
+                    if a == file.text.chars().nth(position).unwrap() {
+                        write!(stdout, "{}{}", color::Fg(color::Green), a);
+                    } else {
+                        write!(stdout, "{}{}", color::Fg(color::Red), a);
                     }
                 }
-                KeyCode::Backspace => {
-                    current_word.pop();
-                    execute!(stdout, cursor::MoveLeft(1), terminal::Clear(terminal::ClearType::UntilNewLine)).unwrap();
-                },
-                KeyCode::Esc => {
-                    println!();
-                    println!();
-                    println!("Quitting...");
-                    break;
-                },
-                _ => ()
+                position += 1;
             }
-        }
+            Key::Backspace => {
+                let (x, y) = stdout.cursor_pos().unwrap();
+                write!(
+                    stdout,
+                    "{}{}{}",
+                    color::Fg(termion::color::Reset),
+                    file.text.chars().nth(x as usize - 1).unwrap(),
+                    termion::cursor::Goto(x - 1, y)
+                );
+                position -= 1;
+            }
+            Key::Ctrl('q') => break,
+            _ => (),
+        };
+        stdout.flush().unwrap();
     }
 }
